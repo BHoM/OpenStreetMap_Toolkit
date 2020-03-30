72fed5bfc7b2f2bf8da0b7402ea91bd61c99d61a
rolyh <roly.hudson@gmail.com>
2020-03-30 10:03:06 +0100

    Remove usings

--- OpenStreetMap_Engine/Convert/UTMtoLatLon.cs
+++ OpenStreetMap_Engine/Convert/UTMtoLatLon.cs
@@ -19,9 +19,6 @@
  * You should have received a copy of the GNU Lesser General Public License     
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
-using System;
-using System.ComponentModel;
-using BH.oM.Reflection.Attributes;
 using CoordinateSharp;
 
 namespace BH.Engine.OpenStreetMap
