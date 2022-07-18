8addd2b7288f3a113b42c4594c94a047eb5dc668
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:06 +0100

    Update Assembly Information

--- OpenStreetMap_oM/Properties/AssemblyInfo.cs
+++ OpenStreetMap_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenStreetMap_Toolkit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/OpenStreetMap_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenStreetMap_Toolkit")]
